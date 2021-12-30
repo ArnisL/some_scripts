0b33f1082f73accce19535c1452e5e0ee883688f
Arnis Lapsa <arnis.lapsa@gmail.com>
2021-12-30 12:12:24 +0200

    Create reflex.c

---
+++ reflex.c
@@ -0,0 +1,27 @@
+var ReFlex(vars Data,int Period)
+{
+	var a1,b1,c1,c2,c3,Slope,DSum;
+	vars Filt = series(Data[0]), MS = series(0);
+	int i;
+	
+//Gently smooth the data in a SuperSmoother
+	a1 = exp(-1.414*2*PI/Period);
+	b1 = 2*a1*cos(1.414*PI/Period);
+	c2 = b1;
+	c3 = -a1*a1;
+	c1 = 1-c2-c3;
+	Filt[0] = c1*(Data[0]+Data[1])/2 + c2*Filt[1] + c3*Filt[2];
+
+//Period is assumed cycle period
+	Slope = (Filt[Period]-Filt[0]) / Period;
+
+//Sum the differences
+	for(i=1,DSum=0; i<=Period; i++)
+		DSum += (Filt[0] + i*Slope) - Filt[i];
+	DSum /= Period;
+	
+//Normalize in terms of Standard Deviations
+	MS[0] = .04*DSum*DSum + .96*MS[1];
+	if(MS[0] > 0.) return DSum/sqrt(MS[0]);
+	else return 0.;
+}
