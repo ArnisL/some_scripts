984e8f3fe9079f2b3aaaaa0edaad7c741c410d70
Arnis Lapsa <arnis.lapsa@gmail.com>
2022-02-14 16:16:51 +0200

    Update eo.c

--- eo.c
+++ eo.c
@@ -9,7 +9,7 @@ var SumSq(vars Data,int Length)
 
 var EO(vars Data,int Length)
 {
-  vars Derivs = series(priceClose(0)-priceClose(2));
+  vars Derivs = series(Data[0]-Data[2]);
   var RMS = sqrt(SumSq(Derivs,Length)/Length);
   var NDeriv = Derivs[0]/RMS;
   vars IFishs = series(FisherInv(&NDeriv));
