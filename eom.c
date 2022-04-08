6441469f3043b82c3f4b1d9d03e1eb4992d3716b
Arnis Lapsa <arnis.lapsa@gmail.com>
2022-04-08 22:51:42 +0300

    Create eom.c

---
+++ eom.c
@@ -0,0 +1,11 @@
+function EOM(var Length, vars High, vars Low, var Volume) {
+	var div = 10000000000;
+	var eom = SMA(
+		series(
+			div * (
+				(High[0]+Low[0])/2 - (High[1]+Low[1])/2
+			) * (High[0] - Low[0]) / Volume
+		),	Length
+	);
+	return eom*1000;
+}
