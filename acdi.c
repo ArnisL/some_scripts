36f5bbc15d6e396b44b8f21ccd2c5a5388ef07c5
Arnis Lapsa <arnis.lapsa@gmail.com>
2022-04-08 22:59:22 +0300

    Create acdi.c

---
+++ acdi.c
@@ -0,0 +1,7 @@
+function ACDI(var Close, var High, var Low, var Volume){
+	vars ad = series(0, 2);
+	var clv = ((Close - Low) - (High - Close)) / (High - Low);
+	var cmfv = clv * Volume;
+	ad[0] = ad[1] + cmfv;
+	return ad[0];
+}
