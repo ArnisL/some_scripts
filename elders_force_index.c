1b3baf2da54b245a36c323028cde7d96b6ffbc09
Arnis Lapsa <arnis.lapsa@gmail.com>
2022-04-16 19:30:49 +0300

    Create elders_force_index.c

---
+++ elders_force_index.c
@@ -0,0 +1,6 @@
+function EFI(int Period, var Close, var Volume) {
+  vars CloseSeries = series(Close);
+  var change = CloseSeries[0] - CloseSeries[1];
+  var efi = EMA(change * Volume, Period);
+  return efi;
+}
