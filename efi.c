6b8b0dbaa8162b107ae02aa06630aabe82b733d9
Arnis Lapsa <arnis.lapsa@gmail.com>
2022-04-22 10:34:07 +0300

    Create efi.c

---
+++ efi.c
@@ -0,0 +1,7 @@
+// Elder's force index
+function EFI(int Period, var Close, var Volume) {
+	vars CloseSeries = series(Close);
+	var change = CloseSeries[0] - CloseSeries[1];
+	var efi = EMA(change * Volume, Period);
+	return efi;
+}
