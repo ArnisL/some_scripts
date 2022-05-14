59bcd329c02b6e03633b57587fe1eb91d37a2af9
Arnis Lapsa <arnis.lapsa@gmail.com>
2022-05-15 02:06:41 +0300

    Update vpt.c

--- vpt.c
+++ vpt.c
@@ -1,4 +1,4 @@
-function VPT(var Volume, vars Close) {
+float VPT(var Volume, vars Close) {
 	vars vpt = series(0, 2);
 	vpt[0] = vpt[1] + Volume * ((Close[0] - Close[1])/Close[1]);
 	return vpt[0];
