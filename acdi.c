ae044508db911a16ed182ef0e14f6ed58b92d90e
Arnis Lapsa <arnis.lapsa@gmail.com>
2022-05-15 02:03:47 +0300

    Update acdi.c

--- acdi.c
+++ acdi.c
@@ -1,4 +1,4 @@
-function ACDI(var Close, var High, var Low, var Volume){
+float ACDI(var Close, var High, var Low, var Volume){
 	vars ad = series(0, 2);
 	var clv = ((Close - Low) - (High - Close)) / (High - Low);
 	var cmfv = clv * Volume;
