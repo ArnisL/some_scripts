eac1a07c77b3437165387efc08259b261530b42b
Arnis Lapsa <arnis.lapsa@gmail.com>
2022-05-15 02:04:03 +0300

    Update cmf.c

--- cmf.c
+++ cmf.c
@@ -1,4 +1,4 @@
-function CMF(var Period, var High, var Low, var Close, var Volume) {
+float CMF(var Period, var High, var Low, var Close, var Volume) {
 	var ad = ((2*Close-Low-High)/(High-Low))*Volume;
-	return Sum(series(ad), Period) / Sum(series(Volume), Period)*100;
+	return Sum(series(ad), Period) / Sum(series(Volume), Period);
 }
