dd5d22fb4de74d94faf88ecf71dbb9565d167a4f
Arnis Lapsa <arnis.lapsa@gmail.com>
2022-05-15 02:06:29 +0300

    Update vo.c

--- vo.c
+++ vo.c
@@ -1,4 +1,4 @@
-function VO(int ShortPeriod, int LongPeriod, vars VolumeSeries) {
+float VO(int ShortPeriod, int LongPeriod, vars VolumeSeries) {
 	var s = EMA(VolumeSeries, ShortPeriod);
 	var l = EMA(VolumeSeries, LongPeriod);
 	return ((s - l) / l) * 100;
