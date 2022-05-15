cbf0fe00cdc822c41c00899cdd4eed2048648984
Arnis Lapsa <arnis.lapsa@gmail.com>
2022-05-15 19:06:28 +0300

    Update acdi.c

--- acdi.c
+++ acdi.c
@@ -1,7 +1,13 @@
 float ACDI(var Close, var High, var Low, var Volume){
-	vars ad = series(0, 2);
-	var clv = ((Close - Low) - (High - Close)) / (High - Low);
-	var cmfv = clv * Volume;
+	vars ad = series(0);
+	var clv;
+	if ((High - Low) == 0) {
+		clv = ((Close - Low) - (High - Close));
+	} else {
+		clv = ((Close - Low) - (High - Close)) / (High - Low);
+	}
+	
+	var cmfv = clv/10000 * Volume;
 	ad[0] = ad[1] + cmfv;
 	return ad[0];
 }
